5e88417161c56c9d8a022ac28b4031e3efe9940f
mokunshao <mokunshao@foxmail.com>
2022-10-03 20:03:04 +0800

    backup

--- src/main/java/com/example/demo/another/StudentMapper.java
+++ src/main/java/com/example/demo/another/StudentMapper.java
@@ -4,6 +4,8 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.util.Date;
+
 @Mapper
 public interface StudentMapper {
 
@@ -13,4 +15,13 @@ public interface StudentMapper {
     @Mapping(source = "birthday", target = "birthday", dateFormat = "yyyy-MM-dd HH:mm:ss")
     StudentVO student2StudentVO(Student student);
 
+    public static void main(String[] args) {
+
+        Student student = Student.builder().name("小明").age(6).gender(GenderEnum.Male).height(121.1).birthday(new Date()).build();
+        System.out.println(student);
+        //这行代码便是实际要用的代码
+        StudentVO studentVO = StudentMapper.INSTANCE.student2StudentVO(student);
+        System.out.println(studentVO);
+
+    }
 }
